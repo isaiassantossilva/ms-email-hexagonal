d8f4fe7104b3402fd0cdc924808dcffa756185a1
isaiassantossilva <isaias661248@gmail.com>
2021-12-07 09:18:07 -0300

    add hello

--- src/main/java/com/santos/email/MsEmailHexagonalApplication.java
+++ src/main/java/com/santos/email/MsEmailHexagonalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsEmailHexagonalApplication {
 
     public static void main(String[] args) {
+        System.out.println("HERE");
         SpringApplication.run(MsEmailHexagonalApplication.class, args);
     }
 
